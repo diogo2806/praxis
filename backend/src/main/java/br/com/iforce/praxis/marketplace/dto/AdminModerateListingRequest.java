df0aa0d26d7239b04a921cfbb06ed51b0607aee2
Diogo Santos <diogo.silva@ibrowse.net.br>
2026-07-01 06:41:35 -0300

    Add listing moderation approval flag

--- backend/src/main/java/br/com/iforce/praxis/marketplace/dto/AdminModerateListingRequest.java
+++ backend/src/main/java/br/com/iforce/praxis/marketplace/dto/AdminModerateListingRequest.java
@@ -3,6 +3,7 @@ package br.com.iforce.praxis.marketplace.dto;
 import jakarta.validation.constraints.Size;
 
 public record AdminModerateListingRequest(
+        Boolean approved,
         @Size(max = 1000) String reason
 ) {
 }
