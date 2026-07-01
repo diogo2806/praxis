adc364523eeb15d29015b1cbf381a15ba996b7ee
Diogo Santos <diogo.silva@ibrowse.net.br>
2026-07-01 06:41:25 -0300

    Add professional moderation approval flag

--- backend/src/main/java/br/com/iforce/praxis/marketplace/dto/AdminModerateProfessionalRequest.java
+++ backend/src/main/java/br/com/iforce/praxis/marketplace/dto/AdminModerateProfessionalRequest.java
@@ -3,6 +3,7 @@ package br.com.iforce.praxis.marketplace.dto;
 import jakarta.validation.constraints.Size;
 
 public record AdminModerateProfessionalRequest(
+        Boolean approved,
         @Size(max = 1000) String reason
 ) {
 }
