537036ca41d5c8d279954a667f9db132581a0a25
Diogo Santos <diogo_2806@hotmail.com>
2026-07-05 17:01:39 -0300

    feat(billing): definir tipos de solicitação comercial

---
+++ backend/src/main/java/br/com/iforce/praxis/billing/model/PlanChangeRequestType.java
@@ -0,0 +1,7 @@
+package br.com.iforce.praxis.billing.model;
+
+/** Ação comercial que uma empresa Enterprise solicita para seu contrato. */
+public enum PlanChangeRequestType {
+    CHANGE_PLAN,
+    CANCEL_CONTRACT
+}
