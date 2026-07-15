afece4d0359dc62386cf3f34e72d97092279ce76
Diogo Santos <diogo_2806@hotmail.com>
2026-07-15 18:29:34 -0300

    test: tornar stubs compartilhados tolerantes

--- backend/src/test/java/br/com/iforce/praxis/candidate/service/CompanyCandidateLinkServiceTest.java
+++ backend/src/test/java/br/com/iforce/praxis/candidate/service/CompanyCandidateLinkServiceTest.java
@@ -20,6 +20,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -39,6 +41,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 class CompanyCandidateLinkServiceTest {
 
     @Mock
