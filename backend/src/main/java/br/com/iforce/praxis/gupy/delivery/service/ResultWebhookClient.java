03f0a6f67e058aac9ad567c78b7084d293c8b47c
Diogo Santos <diogo_2806@hotmail.com>
2026-07-16 00:09:34 -0300

    chore: preservar compatibilidade interna do cliente legado

--- backend/src/main/java/br/com/iforce/praxis/gupy/delivery/service/ResultWebhookClient.java
+++ backend/src/main/java/br/com/iforce/praxis/gupy/delivery/service/ResultWebhookClient.java
@@ -7,4 +7,12 @@ public interface ResultWebhookClient {
     void postResult(String webhookUrl, TestResultResponse testResultResponse);
 
     void postPayload(String webhookUrl, Object payload);
+
+    /**
+     * Executa o callback contratual e devolve o status HTTP confirmado.
+     *
+     * @param callbackUrl URL fornecida pelo ATS
+     * @return status HTTP 2xx devolvido pelo callback
+     */
+    int getCallback(String callbackUrl);
 }
