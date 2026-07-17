776e05cd22d08aed324046b0d72fb3f1aa098d5b
Diogo Santos <diogo_2806@hotmail.com>
2026-07-16 23:38:55 -0300

    remove execução de callback pelo backend

--- backend/src/main/java/br/com/iforce/praxis/gupy/delivery/service/RestClientResultWebhookClient.java
+++ backend/src/main/java/br/com/iforce/praxis/gupy/delivery/service/RestClientResultWebhookClient.java
@@ -1,33 +1,22 @@
 package br.com.iforce.praxis.gupy.delivery.service;
 
 import br.com.iforce.praxis.gupy.dto.TestResultResponse;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
-import java.io.IOException;
-import java.net.HttpURLConnection;
 import java.net.URI;
 
 @Component
 public class RestClientResultWebhookClient implements ResultWebhookClient {
 
     private final RestClient resultRestClient;
-    private final RestClient callbackRestClient;
     private final GupyOutboundUrlValidator outboundUrlValidator;
 
     public RestClientResultWebhookClient(
             RestClient.Builder restClientBuilder,
-            GupyOutboundUrlValidator outboundUrlValidator,
-            @Value("${praxis.gupy.callback.connect-timeout-ms:3000}") int callbackConnectTimeoutMs,
-            @Value("${praxis.gupy.callback.read-timeout-ms:5000}") int callbackReadTimeoutMs
+            GupyOutboundUrlValidator outboundUrlValidator
     ) {
         this.resultRestClient = restClientBuilder.build();
-        this.callbackRestClient = RestClient.builder()
-                .requestFactory(callbackRequestFactory(callbackConnectTimeoutMs, callbackReadTimeoutMs))
-                .build();
         this.outboundUrlValidator = outboundUrlValidator;
     }
 
@@ -39,33 +28,5 @@ public class RestClientResultWebhookClient implements ResultWebhookClient {
                 .body(testResultResponse)
                 .retrieve()
                 .toBodilessEntity();
-    }
-
-    @Override
-    public int getCallback(String callbackUrl) {
-        URI uri = outboundUrlValidator.validate(callbackUrl);
-        ResponseEntity<Void> response = callbackRestClient.get()
-                .uri(uri)
-                .retrieve()
-                .toBodilessEntity();
-        int statusCode = response.getStatusCode().value();
-        if (!response.getStatusCode().is2xxSuccessful()) {
-            throw new CallbackHttpStatusException(statusCode);
-        }
-        return statusCode;
-    }
-
-    private SimpleClientHttpRequestFactory callbackRequestFactory(int connectTimeoutMs, int readTimeoutMs) {
-        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory() {
-            @Override
-            protected void prepareConnection(HttpURLConnection connection, String httpMethod) throws IOException {
-                super.prepareConnection(connection, httpMethod);
-                // Redirecionamentos não são seguidos: cada destino precisa passar pela validação SSRF.
-                connection.setInstanceFollowRedirects(false);
-            }
-        };
-        requestFactory.setConnectTimeout(Math.max(1, connectTimeoutMs));
-        requestFactory.setReadTimeout(Math.max(1, readTimeoutMs));
-        return requestFactory;
     }
 }
