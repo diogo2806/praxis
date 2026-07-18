2ac7639b133d10eba04d6d4707c8377726e6831b
Diogo Santos <diogo_2806@hotmail.com>
2026-07-18 10:27:46 -0300

    fix: remover versão Flyway duplicada

--- backend/src/main/resources/db/migration/V1009__persist_candidate_token_window.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE candidate_attempts ADD COLUMN candidate_token_issued_at TIMESTAMP WITH TIME ZONE;
-UPDATE candidate_attempts SET candidate_token_issued_at = created_at WHERE candidate_token_issued_at IS NULL;
-ALTER TABLE candidate_attempts ALTER COLUMN candidate_token_issued_at SET NOT NULL;
