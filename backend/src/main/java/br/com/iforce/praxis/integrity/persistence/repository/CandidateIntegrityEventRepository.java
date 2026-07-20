1ac702ea07cdea22699baf3ec34e232d858b7220
Diogo Santos <diogo_2806@hotmail.com>
2026-07-20 19:44:29 -0300

    feat(integrity): habilitar persistência idempotente no repositório

--- backend/src/main/java/br/com/iforce/praxis/integrity/persistence/repository/CandidateIntegrityEventRepository.java
+++ backend/src/main/java/br/com/iforce/praxis/integrity/persistence/repository/CandidateIntegrityEventRepository.java
@@ -3,7 +3,8 @@ package br.com.iforce.praxis.integrity.persistence.repository;
 import br.com.iforce.praxis.integrity.persistence.entity.CandidateIntegrityEventEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CandidateIntegrityEventRepository extends JpaRepository<CandidateIntegrityEventEntity, Long> {
+public interface CandidateIntegrityEventRepository
+        extends JpaRepository<CandidateIntegrityEventEntity, Long>, CandidateIntegrityEventAtomicRepository {
 
     boolean existsBySessionIdAndSequenceNumber(String sessionId, Long sequenceNumber);
 }
