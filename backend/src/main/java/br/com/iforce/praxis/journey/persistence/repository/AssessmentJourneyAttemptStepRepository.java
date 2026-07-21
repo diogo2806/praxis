8f7689179ee2246c195cc65f9180cdced8e71778
Diogo Santos <diogo_2806@hotmail.com>
2026-07-20 23:20:22 -0300

    feat(participations): identify journey child attempts

--- backend/src/main/java/br/com/iforce/praxis/journey/persistence/repository/AssessmentJourneyAttemptStepRepository.java
+++ backend/src/main/java/br/com/iforce/praxis/journey/persistence/repository/AssessmentJourneyAttemptStepRepository.java
@@ -1,14 +1,22 @@
 package br.com.iforce.praxis.journey.persistence.repository;
 
 import br.com.iforce.praxis.journey.persistence.entity.AssessmentJourneyAttemptStepEntity;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
-
+import java.util.Set;
 
 public interface AssessmentJourneyAttemptStepRepository extends JpaRepository<AssessmentJourneyAttemptStepEntity, Long> {
 
     Optional<AssessmentJourneyAttemptStepEntity> findByEmpresaIdAndId(String empresaId, Long id);
+
+    @Query("""
+            SELECT step.candidateAttemptId
+            FROM AssessmentJourneyAttemptStepEntity step
+            WHERE step.empresaId = :empresaId
+              AND step.candidateAttemptId IS NOT NULL
+            """)
+    Set<String> findCandidateAttemptIdsByEmpresaId(@Param("empresaId") String empresaId);
 }
