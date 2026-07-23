b31bb9a168b0ffc20d40bbdb9a5a669ba3337de5
Diogo Santos <diogo_2806@hotmail.com>
2026-07-23 08:40:53 -0300

    feat(integrity): add review audit actions

---
+++ backend/src/main/java/br/com/iforce/praxis/integrity/model/IntegrityReviewAuditAction.java
@@ -0,0 +1,8 @@
+package br.com.iforce.praxis.integrity.model;
+
+public enum IntegrityReviewAuditAction {
+    QUEUE_CREATED,
+    EVIDENCE_ACCESSED,
+    DECISION_RECORDED,
+    EVIDENCE_DISCARDED
+}
