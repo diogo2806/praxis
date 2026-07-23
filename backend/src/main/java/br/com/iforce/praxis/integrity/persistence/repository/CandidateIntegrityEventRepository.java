d091825afe20c87d39b403d85c3b1d33feda1fb3
Diogo Santos <diogo_2806@hotmail.com>
2026-07-23 08:41:36 -0300

    feat(integrity): query technical evidence for reviews

--- backend/src/main/java/br/com/iforce/praxis/integrity/persistence/repository/CandidateIntegrityEventRepository.java
+++ backend/src/main/java/br/com/iforce/praxis/integrity/persistence/repository/CandidateIntegrityEventRepository.java
@@ -1,10 +1,31 @@
 package br.com.iforce.praxis.integrity.persistence.repository;
 
+import br.com.iforce.praxis.integrity.model.IntegrityEventType;
 import br.com.iforce.praxis.integrity.persistence.entity.CandidateIntegrityEventEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface CandidateIntegrityEventRepository
         extends JpaRepository<CandidateIntegrityEventEntity, Long>, CandidateIntegrityEventAtomicRepository {
 
     boolean existsBySessionIdAndSequenceNumber(String sessionId, Long sequenceNumber);
+
+    long countByEmpresaIdAndCandidateAttemptIdAndEventType(
+            String empresaId,
+            String candidateAttemptId,
+            IntegrityEventType eventType
+    );
+
+    List<CandidateIntegrityEventEntity> findByEmpresaIdAndCandidateAttemptIdOrderByOccurredAtAscIdAsc(
+            String empresaId,
+            String candidateAttemptId
+    );
+
+    Page<CandidateIntegrityEventEntity> findByEmpresaIdOrderByOccurredAtAscIdAsc(
+            String empresaId,
+            Pageable pageable
+    );
 }
