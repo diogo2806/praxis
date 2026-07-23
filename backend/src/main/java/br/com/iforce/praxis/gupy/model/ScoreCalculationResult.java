7246008b5cd86edf62106161efdd288e3a9337f2
Diogo Santos <diogo_2806@hotmail.com>
2026-07-23 08:38:33 -0300

    feat(scoring): expose raw and normalized snapshot

--- backend/src/main/java/br/com/iforce/praxis/gupy/model/ScoreCalculationResult.java
+++ backend/src/main/java/br/com/iforce/praxis/gupy/model/ScoreCalculationResult.java
@@ -2,13 +2,38 @@ package br.com.iforce.praxis.gupy.model;
 
 import java.util.List;
 
-
 public record ScoreCalculationResult(
         int score,
+        int rawScore,
+        int pathMaximumScore,
+        int normalizedScore,
+        String scoringAlgorithmVersion,
         List<ResultItem> resultItems,
         boolean humanReviewRequired,
         ReliabilityLevel reliabilityLevel,
         String auditTrail,
         ResultDecision decision
 ) {
+
+    public ScoreCalculationResult(
+            int score,
+            List<ResultItem> resultItems,
+            boolean humanReviewRequired,
+            ReliabilityLevel reliabilityLevel,
+            String auditTrail,
+            ResultDecision decision
+    ) {
+        this(
+                score,
+                score,
+                100,
+                score,
+                "legacy-path-normalized-v1",
+                resultItems,
+                humanReviewRequired,
+                reliabilityLevel,
+                auditTrail,
+                decision
+        );
+    }
 }
