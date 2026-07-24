742da45849bf5122f5166f406a5cedd93245e451
Diogo Santos <diogo_2806@hotmail.com>
2026-07-24 06:51:25 -0300

    feat(quality): aplicar auditoria universal nas novas tabelas

---
+++ backend/src/main/java/db/migration/V1107__refresh_universal_table_auditing.java
@@ -0,0 +1,5 @@
+package db.migration;
+
+/** Reaplica a auditoria universal após a criação das tabelas de qualidade psicométrica. */
+public class V1107__refresh_universal_table_auditing extends V1011__universal_table_auditing {
+}
